8a27bb0aad6560725f1f7bff8b3d736ef4813f44
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-05 14:31:29 +0900

    method to request verses of chapter from getbible v2 api

--- bible-crawler/getbiblev2_api.py
+++ bible-crawler/getbiblev2_api.py
@@ -6,3 +6,8 @@ def request_chapters_of_book(book_id):
     request_url = BASE_URL + "/" + str(book_id) + "/chapters.json"
     response = requests.get(request_url)
     return response.text
+
+def request_verses_of_chapter(book_id, chapter_num):
+    request_url = BASE_URL + "/" + str(book_id) + "/" + str(chapter_num) + ".json"
+    response = requests.get(request_url)
+    return response.text
