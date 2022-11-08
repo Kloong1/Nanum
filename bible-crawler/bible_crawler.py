cebe758b189d135df4a8ffc80461fb038620f4ff
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-08 23:43:51 +0900

    init BibleRepository and call method that save verses of book

--- bible-crawler/bible_crawler.py
+++ bible-crawler/bible_crawler.py
@@ -1,22 +1,39 @@
 import getbiblev2_api as getbible
 import parser
+from BlankVerseManager import BlankVerseManager
+from BibleRepository import BibleRepository
 
-#  response = requests.get("https://getbible.net/v2/korean/1/1.json")
-#  dict = json.loads(response.text)
-#
-#  print(dict)
+def request_verses_of_book(book_id, max_chapter):
+    verses_of_book = []
 
-#  for key in dict.keys():
-#      print(key)
+    for chapter_num in range(1, max_chapter + 1):
+        verses_json = getbible.request_verses_of_chapter(book_id, chapter_num)
+        verses_of_chapter = parser.extract_verses(verses_json)
+        verses_of_book.append(verses_of_chapter)
+
+    return verses_of_book
 
 def main():
     first_book_id = 1
     last_book_id = 66
 
+    bibleRepository = BibleRepository()
+    bibleRepository.connect("./db/db_connect.json")
+
     for book_id in range(first_book_id, last_book_id + 1):
         chapters_json = getbible.request_chapters_of_book(book_id)
         max_chapter = parser.extract_max_chapter(chapters_json)
+
+        verses_of_book = request_verses_of_book(book_id, max_chapter)
+
+        if BlankVerseManager.has_blank_verses_in_book(book_id):
+            BlankVerseManager.fill_blank_verse_in_book(book_id, verses_of_book)
+
+        bibleRepository.save_verses_of_book(book_id, max_chapter, verses_of_book)
+
         break
+    
+    bibleRepository.close()
 
 
 if __name__ == "__main__":
