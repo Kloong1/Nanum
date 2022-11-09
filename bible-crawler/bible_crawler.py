1ce1353761579574d160cb7d927cae7c6ef2da5c
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-09 21:10:36 +0900

    print log

--- bible-crawler/bible_crawler.py
+++ bible-crawler/bible_crawler.py
@@ -17,22 +17,32 @@ def main():
     first_book_id = 1
     last_book_id = 66
 
+    print("Connect to db...")
+
     bibleRepository = BibleRepository()
     bibleRepository.connect("./db/db_connect.json")
 
+    print("Connected.")
+
     for book_id in range(first_book_id, last_book_id + 1):
+        print()
+        print(f"Book id = {book_id}")
+
+        print("Request chapter information...")
         chapters_json = getbible.request_chapters_of_book(book_id)
         max_chapter = parser.extract_max_chapter(chapters_json)
 
+        print("Request verses of book...")
         verses_of_book = request_verses_of_book(book_id, max_chapter)
 
         if BlankVerseManager.has_blank_verses_in_book(book_id):
+            print("Fill in blank verses...")
             BlankVerseManager.fill_blank_verse_in_book(book_id, verses_of_book)
 
+        print("Save verses in db...")
         bibleRepository.save_verses_of_book(book_id, max_chapter, verses_of_book)
-
-        break
     
+    print("Disconnect to db...")
     bibleRepository.close()
 
 
