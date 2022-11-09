33250a5fcd13bac9a3a5636d8d679e6887ae6e29
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-09 21:10:15 +0900

    save chapter info and verse

--- bible-crawler/BibleRepository.py
+++ bible-crawler/BibleRepository.py
@@ -32,11 +32,37 @@ class BibleRepository:
     def save_verses_of_book(self, book_id, max_chapter, verses_of_book):
         self.__save_book_info(book_id, max_chapter)
 
+        for chapter_num in range(1, max_chapter + 1):
+            self.__save_chapter_info(book_id, chapter_num, verses_of_book)
+            self.__save_verses_of_chapter(book_id, chapter_num, verses_of_book[chapter_num - 1])
+
     def __save_book_info(self, book_id, max_chapter):
         sql = "insert into bible_book(book_id, book_max_chapter) values(%s, %s)"
         params = (book_id, max_chapter)
 
         cursor = self.conn.cursor()
         cursor.execute(sql, params)
+
+        self.conn.commit()
+
+    def __save_chapter_info(self, book_id, chapter_num, verses_of_book):
+        chapter_max_verse = len(verses_of_book[chapter_num - 1])
+
+        sql = "insert into bible_chapter(chapter_num, chapter_book_id, chapter_max_verse) values(%s, %s, %s)"
+        params = (chapter_num, book_id, chapter_max_verse)
+
+        cursor = self.conn.cursor()
+        cursor.execute(sql, params)
+
+        self.conn.commit()
+
+    def __save_verses_of_chapter(self, book_id, chapter_num, verses_of_chapter):
+        sql = "insert into bible_verse(verse_book_id, verse_chapter_num, verse_num, verse_content) values(%s, %s, %s, %s)"
+
+        cursor = self.conn.cursor()
+
+        for verse in verses_of_chapter:
+            params = (book_id, chapter_num, verse[0], verse[1])
+            cursor.execute(sql, params)
 
         self.conn.commit()
