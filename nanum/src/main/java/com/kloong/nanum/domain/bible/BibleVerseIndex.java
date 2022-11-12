0a1c2fb7d797ee57c282dcbf3a14a2cb96606cae
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-12 17:06:46 +0900

    성경 단락, 성경 구절 인덱스 도메인 객체

--- nanum/src/main/java/com/kloong/nanum/domain/bible/BibleVerseIndex.java
+++ nanum/src/main/java/com/kloong/nanum/domain/bible/BibleVerseIndex.java
@@ -1,2 +1,18 @@
-package com.kloong.nanum.domain.bible;public class BibleVerseIndex {
+package com.kloong.nanum.domain.bible;
+
+import lombok.Data;
+
+@Data
+public class BibleVerseIndex {
+    private int bookId;
+    private int chapter;
+    private int verse;
+    private int seq;
+
+    public BibleVerseIndex(int bookId, int chapter, int verse, int seq) {
+        this.bookId = bookId;
+        this.chapter = chapter;
+        this.verse = verse;
+        this.seq = seq;
+    }
 }
