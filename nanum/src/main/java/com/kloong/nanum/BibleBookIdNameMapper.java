58d7f5f8472cbfc97cdfa06d66213213908260c9
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-12 17:07:49 +0900

    성경 이름을 id로 mapping 하는 기능 추가

--- nanum/src/main/java/com/kloong/nanum/BibleBookIdNameMapper.java
+++ nanum/src/main/java/com/kloong/nanum/BibleBookIdNameMapper.java
@@ -1,5 +1,6 @@
-package com.kloong.nanum.domain.bible;
+package com.kloong.nanum;
 
+import com.kloong.nanum.constant.bible.BibleConst;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
@@ -7,12 +8,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class BibleBookIdNameMapper {
 
-    private final String[] bibleBookNames = new String[67];
+    private final Map<Integer, String> idToName = new HashMap<>(BibleConst.BOOK_MAX_ID * 2);
+    private final Map<String, Integer> nameToId = new HashMap<>(BibleConst.BOOK_MAX_ID * 2);
 
     public BibleBookIdNameMapper() {
         initBibleBookNames();
@@ -29,15 +33,23 @@ public class BibleBookIdNameMapper {
             throw new IllegalStateException("Error! 성경 이름 파일을 찾을 수 없습니다.", e);
         }
 
-        for (int id = 1; id < bibleBookNames.length; id++) {
-            bibleBookNames[id] = bibleBookNameList.get(id - 1);
+        for (int id = BibleConst.BOOK_MIN_ID; id <= BibleConst.BOOK_MAX_ID; id++) {
+            idToName.put(id, bibleBookNameList.get(id - 1));
+            nameToId.put(bibleBookNameList.get(id - 1), id);
         }
     }
 
     public String getBibleBookName(int bookId) {
-        if (bookId < 1 || bookId > 66) {
-            throw new IllegalArgumentException("Error! bookId 범위는 1~66 이어야합니다.");
+        if (!idToName.containsKey(bookId)) {
+            throw new IllegalArgumentException("Error! 잘못된 bookId 입니다.");
         }
-        return bibleBookNames[bookId];
+        return idToName.get(bookId);
+    }
+
+    public int getBibleBookId(String bookName) {
+        if (!nameToId.containsKey(bookName)) {
+            throw new IllegalArgumentException("Error! 잘못된 bookName 입니다.");
+        }
+        return nameToId.get(bookName);
     }
 }
